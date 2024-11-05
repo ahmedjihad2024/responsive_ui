137da07967ed9102ce8a4a5db75b1e510ca235cc
= <mood27358@gmail.com>
2024-11-05 06:36:54 +0200

    UPDATE adaptive_widgets.dart

--- lib/dynamic/adaptive_widgets.dart
+++ lib/dynamic/adaptive_widgets.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:responsive_ui/responsive_ui.dart';
 
@@ -7,18 +9,31 @@ class AdaptScreen extends StatelessWidget {
   final Widget Function(BuildContext context)? mobile;
   final Widget Function(BuildContext context)? tablet;
   final Widget Function(BuildContext context)? desktop;
-  const AdaptScreen({super.key, this.mobile, this.tablet, this.desktop});
+  final Widget Function(BuildContext context)? windows;
+
+  const AdaptScreen({
+    Key? key,
+    this.mobile,
+    this.tablet,
+    this.desktop,
+    this.windows,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if(deviceDetails.deviceType == DEVICE_SIZE_TYPE.MOBILE){
-      return mobile?.call(context) ?? SizedBox.shrink();
-    }else if(deviceDetails.deviceType == DEVICE_SIZE_TYPE.TABLET){
-      return tablet?.call(context) ?? SizedBox.shrink();
-    }else if(deviceDetails.deviceType == DEVICE_SIZE_TYPE.DESKTOP){
-      return desktop?.call(context) ?? SizedBox.shrink();
-    }else{
-      return SizedBox.shrink();
+    if (Platform.isWindows && windows != null) {
+      return windows?.call(context) ?? const SizedBox.shrink();
+    }
+
+    // Adapt to device type for non-Windows platforms
+    if (deviceDetails.deviceType == DEVICE_SIZE_TYPE.MOBILE) {
+      return mobile?.call(context) ?? const SizedBox.shrink();
+    } else if (deviceDetails.deviceType == DEVICE_SIZE_TYPE.TABLET) {
+      return tablet?.call(context) ?? const SizedBox.shrink();
+    } else if (deviceDetails.deviceType == DEVICE_SIZE_TYPE.DESKTOP) {
+      return desktop?.call(context) ?? const SizedBox.shrink();
+    } else {
+      return const SizedBox.shrink();
     }
   }
 }
@@ -28,6 +43,9 @@ class AdaptOrientation extends StatelessWidget {
   final Widget Function(BuildContext context)? mobileLand;
   final Widget Function(BuildContext context)? tabletPort;
   final Widget Function(BuildContext context)? tabletLand;
+  final Widget Function(BuildContext context)? desktopPort;
+  final Widget Function(BuildContext context)? desktopLand;
+  final Widget Function(BuildContext context)? windows;
 
   const AdaptOrientation({
     Key? key,
@@ -35,23 +53,38 @@ class AdaptOrientation extends StatelessWidget {
     this.mobileLand,
     this.tabletPort,
     this.tabletLand,
+    this.desktopPort,
+    this.desktopLand,
+    this.windows,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    if (Platform.isWindows && windows != null) {
+      return windows?.call(context) ?? const SizedBox.shrink();
+    }
+
+    // Determine device orientation and type
     final Orientation orientation = MediaQuery.of(context).orientation;
     final bool isMobile = deviceDetails.deviceType == DEVICE_SIZE_TYPE.MOBILE;
     final bool isTablet = deviceDetails.deviceType == DEVICE_SIZE_TYPE.TABLET;
+    final bool isDesktop = deviceDetails.deviceType == DEVICE_SIZE_TYPE.DESKTOP;
 
     if (orientation == Orientation.portrait) {
-      if (isTablet) {
-        return (tabletPort ?? mobilePort)?.call(context) ?? const SizedBox.shrink();
+      // Portrait Mode
+      if (isDesktop) {
+        return desktopPort?.call(context) ?? tabletPort?.call(context) ?? mobilePort?.call(context) ?? const SizedBox.shrink();
+      } else if (isTablet) {
+        return tabletPort?.call(context) ?? mobilePort?.call(context) ?? const SizedBox.shrink();
       } else if (isMobile) {
         return mobilePort?.call(context) ?? const SizedBox.shrink();
       }
     } else {
-      if (isTablet) {
-        return (tabletLand ?? mobileLand)?.call(context) ?? const SizedBox.shrink();
+      // Landscape Mode
+      if (isDesktop) {
+        return desktopLand?.call(context) ?? tabletLand?.call(context) ?? mobileLand?.call(context) ?? const SizedBox.shrink();
+      } else if (isTablet) {
+        return tabletLand?.call(context) ?? mobileLand?.call(context) ?? const SizedBox.shrink();
       } else if (isMobile) {
         return mobileLand?.call(context) ?? const SizedBox.shrink();
       }
